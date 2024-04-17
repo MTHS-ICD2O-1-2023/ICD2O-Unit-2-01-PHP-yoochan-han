1ddba6183b4da30c0651af6df3acca37f4d065cb
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-04-18 07:08:36 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,29 @@
 <!DOCTYPE html>
-  <html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <head>
-    <title>Title</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <title>My first css</title>
   </head>
-  <body>
-    <?php echo '<p>Hello, World!, in PHP</p>'; ?>
-  </body>
+<body>
+  <header class="myheader">
+    <h1> My webpage header </h1>
+    <h3> this webpage is css testing place</h3>
+</header>
+<div class="right-img">
+  <img src="bsod.png" alt="blue_screen">
+</div>
+  <div class="para">
+    <div>
+      <p>log : my computer died at 10:26</p>
+      <p>log : my computer came back to life at 10:34</p>
+      <p>log : work1 finished at 10:40</p>
+      <p>log : work2 finished at 11:23</p>
+    </div>
+    <a href="https://mths-icd2o-1-2023.github.io/ICD2O-Unit-1-04-yoochan-han/index2.html">link to other page</a>
+  </div>
+</body>
 </html>
+
