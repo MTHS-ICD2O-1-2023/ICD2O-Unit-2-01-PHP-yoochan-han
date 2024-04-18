329257b4ddc92f4e7f9efff30f58a5961467462b
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-04-18 20:14:38 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,48 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <head>
-    <link rel="stylesheet" href="./css/style.css">
-    <title>My first css</title>
-  </head>
+ <meta charset="utf-8" />
+ <meta name="description" content="My first webpage, with js" />
+ <meta name="keywords" content="mths, ics2o" />
+ <meta name="author" content="Yoochan Han" />
+ <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+ <link rel="stylesheet" href="./css/style.css" />
+ <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+ <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css" />
+ <link rel="manifest" href="site.webmanifest" />
+ <title>My first webpage, with JavaScript</title>
+</head>
 <body>
-  <header class="myheader">
-    <h1> My webpage header </h1>
-    <h3> this webpage is css testing place</h3>
-</header>
-<div class="right-img">
-  <img src="bsod.png" alt="blue_screen">
-</div>
-  <div class="para">
-    <div>
-      <p>log : my computer died at 10:26</p>
-      <p>log : my computer came back to life at 10:34</p>
-      <p>log : work1 finished at 10:40</p>
-      <p>log : work2 finished at 11:23</p>
-    </div>
-    <a href="https://mths-icd2o-1-2023.github.io/ICD2O-Unit-1-04-yoochan-han/index2.html">link to other page</a>
-  </div>
+<?php
+echo '<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>';
+echo '<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">';
+echo '<header class="mdl-layout__header">';
+echo '<div class="mdl-layout__header-row">';
+echo '<span class="mdl-layout-title">My first webpage, with JavaScript</span>';
+echo '</div>';
+echo '</header>';
+echo '<main class="mdl-layout__content">';
+echo '<div class="right-image">';
+echo '<img src="./images/notebook-animation.svg" alt="laptop image" class="myimg" />';
+echo '</div>';
+echo '<br />';
+echo '<script>function myButtonClicked() {';
+echo 'document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"; }';
+echo '</script>';
+echo '<!-- Accent-colored raised button with ripple -->';
+echo '<div id="mybutton">';
+echo '<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"';
+echo 'onclick="myButtonClicked()">';
+echo 'Say Hello, World!';
+echo '</button>';
+echo '</div>';
+echo '<br />';
+echo '<br />';
+echo '<div id="page-content-answer">';
+echo '<div id="hello-world"></div>';
+echo '</div>';
+echo '</main>';
+echo '</div>';
+?>
 </body>
-</html>
-
+</html>  
